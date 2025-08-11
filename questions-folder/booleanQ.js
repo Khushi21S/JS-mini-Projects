c106b7a50b4b09c61e65915550cf2da013b156f0
Khushi Sharma <87928896+Khushi21S@users.noreply.github.com>
2025-08-12 03:02:45 +0530

    added basic questions folder

---
+++ questions-folder/booleanQ.js
@@ -0,0 +1,12 @@
+//Truthy or Falsy Value
+
+// Convert Number to Boolean
+// Here, we declare the variable num and assign it the value 5. We also declare the variable bool which we assign the boolean representation of num.
+// Extend the code such that the console.log() statement logs false.
+
+let num = 5;
+let bool = Boolean(num);
+
+console.log(!bool);
+
+
