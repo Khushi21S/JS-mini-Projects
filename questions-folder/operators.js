ee897145032eae6e8b35468a724737ec76a24ffa
Khushi Sharma <87928896+Khushi21S@users.noreply.github.com>
2025-08-13 19:24:42 +0530

    added operators questions

--- questions-folder/operators.js
+++ questions-folder/operators.js
@@ -1,9 +1,34 @@
-// Comparison operators - Greater than
-// In the console.log() statement below we use the Greater Than operator to check whether the value of numOne is greater 
-// than the value of numTwo. Change the code so that the console.log() statement logs true.
+// Arithmetic operators - Addition
+// In the code below we calculate the sum of numOne and numTwo with the Addition operator +. 
+// Then, the console.log() statement checks whether the sum equals 10. Change the code so that the console.log() statement logs true.
 
-let numOne = 9;
-let numTwo = 7;
+// let numOne = 3;
+// let numTwo = 7;
 
-console.log(numOne > numTwo);
+// console.log(numOne + numTwo === 10 );
+
+// Arithmetic operators - Subtraction
+// In the code below we calculate the difference of numOne and numTwo with the Substraction operator -. 
+// Then, the console.log() statement checks whether the difference equals 5. Change the code so that the console.log() 
+// statement logs true.
+
+// Arithmetic operators - Exponential
+// In the code below we calculate numOne to the power of numTwo with the Exponential operator **.
+//  Then, the console.log() statement checks whether the result of this calculation equals 8. Change
+//   the code so that the console.log() statement logs true.
+
+// let numOne = 3;
+// let numTwo = 2;
+
+// console.log(numTwo ** numOne === 8 );
+
+// Arithmetic operators - Remainder
+// In the code below we use the Remainder operator to calculate the remainder of numOne divided by numTwo.
+//  The console.log() statement expects the remainder to be 3. Change the code so that the remainder is 3.
+
+
+let numOne = 18;
+let numTwo = 5;
+
+console.log(numOne % numTwo === 3 );
 
